eecfec90b683742fa2644b3a5a056d4e662250c3
Gabriel Tan <gabtan99@gmail.com>
2020-12-13 23:54:33 +0800

    feat: bat file to compile mvc

---
+++ compmvc.bat
@@ -0,0 +1,8 @@
+javac --module-path lib\javafx-sdk-11.0.2\lib --add-modules javafx.controls -cp ".;lib/richtextfx-fat-0.10.5.jar;lib/antlr-4.9-complete.jar"  model/*.java
+
+
+javac --module-path lib\javafx-sdk-11.0.2\lib --add-modules javafx.controls -cp ".;lib/richtextfx-fat-0.10.5.jar;lib/antlr-4.9-complete.jar"  controller/*.java
+
+
+javac --module-path lib\javafx-sdk-11.0.2\lib --add-modules javafx.controls -cp ".;lib/richtextfx-fat-0.10.5.jar;lib/antlr-4.9-complete.jar"  view/*.java
+
