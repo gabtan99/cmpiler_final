064ae0655616c7135e4e7829daecdea4906f5ff2
Gabriel Tan <gabtan99@gmail.com>
2020-12-15 18:42:43 +0800

    refactor: cleaned code

--- model/PSCErrorGenerator.java
+++ model/PSCErrorGenerator.java
@@ -3,41 +3,41 @@ package model;
 public class PSCErrorGenerator {
 
     // default ANTLR template messages
-    public static final String EXTRANEOUS_ERR = "extraneous input";
-    public static final String MISSING_ERR = "missing";
-    public static final String MISMATCHED_ERR = "mismatched input";
-    public static final String NO_VIABLE_ERR = "no viable alternative at input";
-    public static final String TOKEN_RECOGNITION_ERR = "token recognition error";
+    private static final String EXTRANEOUS_ERR = "extraneous input";
+    private static final String MISSING_ERR = "missing";
+    private static final String MISMATCHED_ERR = "mismatched input";
+    private static final String NO_VIABLE_ERR = "no viable alternative at input";
+    private static final String TOKEN_RECOGNITION_ERR = "token recognition error";
+
+    private static final String INT_CONST_TOK = "INTEGERCONSTANT";
+    private static final String FLOAT_CONST_TOK = "FLOATINGCONSTANT";
+    private static final String BOOL_CONST_TOK = "BOOLCONSTANT";
+    private static final String STRING_TOK = "StringLiteral";
+    private static final String IDENTIFIER_TOK = "IDENTIFIER";
 
     public String generateMsg(int line, int charPositionInLine, String msg) {
 
         PSCError err = new PSCError(line, charPositionInLine);
 
         if (msg.contains(NO_VIABLE_ERR)) { 
-
             String strList[] = msg.split(NO_VIABLE_ERR);
             err.setMsg("Could not resolve the token " + strList[1] + ".");
-
         } else if (msg.contains(MISMATCHED_ERR)) {
 
             String strList[] = msg.split(MISMATCHED_ERR);
-            String message[] = new String[1];
+            String tokList[] = splitTokens(strList[1]);
+            String offending = tokList[0];
+            String alts = tokList[1];
 
-            if (strList[1].contains("expecting")) {
-                message = strList[1].split("expecting");
-            }
-
-            if (message[1].contains("INTEGERCONSTANT") && message[1].contains("FLOATINGCONSTANT") && message[1].contains("BOOLCONSTANT") && message[1].contains("StringLiteral") && message[1].contains("IDENTIFIER")) {
-                err.setMsg("Mismatched input " + message[0] + ". Try replacing it with an expression.");
-            } else if (message[1].contains("IDENTIFIER")) {
+            if (alts.contains(INT_CONST_TOK) && alts.contains(FLOAT_CONST_TOK) && alts.contains(BOOL_CONST_TOK) && alts.contains(STRING_TOK) && alts.contains(IDENTIFIER_TOK)) {
+                err.setMsg("Mismatched input " + offending + ". Try replacing it with an expression.");
+            } else if (alts.contains(IDENTIFIER_TOK)) {
                 err.setMsg("Expecting an identifier");
             } else {
-                err.setMsg("Mismatched input " + message[0] + " Try replacing it with " + message[1]);
+                err.setMsg("Mismatched input " + offending + " Try replacing it with " + alts);
             }
 
-
         } else if (msg.contains(MISSING_ERR)) {
-
             String strList[] = msg.split(MISSING_ERR);
             String tokens[] = strList[1].split("at");
 
@@ -45,27 +45,23 @@ public class PSCErrorGenerator {
         } else if (msg.contains(EXTRANEOUS_ERR)) {
 
             String strList[] = msg.split(EXTRANEOUS_ERR);
-            String message[] = new String[1];
-
+            String tokList[] = new String[1];
 
             for (int i = 0; i < strList.length; i++) { 
-                strList[i] = strList[i].trim();
-
-                if (strList[i].contains("expecting")) {
-                    message = strList[i].split("expecting");
-                }
+                tokList = splitTokens(strList[i].trim());
             }
-            
-            if (message[1].contains("INTEGERCONSTANT") && message[1].contains("FLOATINGCONSTANT") && message[1].contains("BOOLCONSTANT") && message[1].contains("StringLiteral") && message[1].contains("IDENTIFIER")) {
-                err.setMsg("Extraneous Input : Consider removing " + message[0] + " and replacing it with an expression.");
+            String offending = tokList[0];
+            String alts = tokList[1];
 
+            if (alts.contains(INT_CONST_TOK) && alts.contains(FLOAT_CONST_TOK) && alts.contains(BOOL_CONST_TOK) && alts.contains(STRING_TOK) && alts.contains(IDENTIFIER_TOK)) {
+                err.setMsg("Extraneous Input : Consider removing " + offending + " and replacing it with an expression.");
             } 
-            else if (message[1].contains("IDENTIFIER") && message[1].contains("}")) {
+            else if (alts.contains(IDENTIFIER_TOK) && alts.contains("}")) {
                 err.setMsg("Extraneous Input : Consider matching all previous bracket pairs.");
-            }else if (message[1].contains("IDENTIFIER"))  {
-                err.setMsg("Extraneous Input : Consider removing " + message[0] + " and replacing it with an identifier.");
+            }else if (alts.contains(IDENTIFIER_TOK))  {
+                err.setMsg("Extraneous Input : Consider removing " + offending + " and replacing it with an identifier.");
             } else {
-                err.setMsg("Extraneous Input : Consider removing " + message[0] + " and replacing it with " + message[1] + ".");
+                err.setMsg("Extraneous Input : Consider removing " + offending + " and replacing it with " + alts + ".");
             }
 
         } else if (msg.contains(TOKEN_RECOGNITION_ERR)) {
@@ -73,6 +69,17 @@ public class PSCErrorGenerator {
         }
 
         return err.getFullError();
+    }
+
+
+    private String[] splitTokens(String str) {
+        String list[] = new String[1];
+
+        if (str.contains("expecting")) {
+            list = str.split("expecting");
+        }
+
+        return list;
     }
 
 }
